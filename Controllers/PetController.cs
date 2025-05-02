765752dba64d1e1e31b5a7e2b5aeda9e4f7db2e4
Abrahansito <huamancarlosabraham@gmail.com>
2025-05-02 07:23:26 -0500

    Asignación de adopciones y listado de adopciones

---
+++ Controllers/PetController.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PC2Programacion.Data; 
+
+namespace PC2Programacion.Controllers
+{
+    
+    public class PetController : Controller
+    {
+        private readonly ILogger<PetController> _logger;
+         private readonly ApplicationDbContext _context;
+
+        public PetController(ILogger<PetController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+    public IActionResult CrearAdopcion()
+    {
+        
+        ViewBag.Mascotas = _context.Pet.Where(p => p.EstadoAdopcion == true).ToList();
+        ViewBag.Adoptantes = _context.Adopter.ToList();
+
+        return View();
+    }
+
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult CrearAdopcion(Adoption adoption)
+    {
+        if (ModelState.IsValid)
+        {
+        
+            var pet = _context.Pet.FirstOrDefault(p => p.Id == adoption.PetId);
+            if (pet != null)
+            {
+                pet.EstadoAdopcion = false;
+                _context.Pet.Update(pet);
+            }
+
+            
+            _context.Adoption.Add(adoption);
+            _context.SaveChanges();
+
+            TempData["mensaje"] = "Adopción registrada correctamente.";
+            return RedirectToAction("ListaAdopciones");
+        }
+        return View(adoption);
+    }
+
+
+    public IActionResult ListaAdopciones()
+    {
+        var adopciones = _context.Adoption
+            .Include(a => a.Pet)
+            .Include(a => a.Adopter)
+            .ToList();
+
+        return View(adopciones);
+    }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
